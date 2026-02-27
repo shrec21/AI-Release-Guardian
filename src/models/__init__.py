0ed38d68122afb8b9d78e2ebb1a709489e5f491c
Shrec21 <shreyaschougule1357@gmail.com>
2026-02-27 16:53:49 -0600

    connected all models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,58 @@
-"""Data models module."""
+"""Data models module.
+
+This module re-exports all Pydantic models for easy importing:
+    from src.models import BuildInfo, TestAnalysisReport, RiskAssessment, ...
+"""
+
+from src.models.build_metadata import BuildInfo, GitDiff, PipelineStatus
+from src.models.perf_report import (
+    BenchmarkResult,
+    MetricComparison,
+    PerformanceAnalysisReport,
+    SLOCheck,
+)
+from src.models.release_decision import (
+    AuditRecord,
+    NotificationRecord,
+    PolicyEvaluation,
+    ReleaseDecision,
+)
+from src.models.risk_assessment import (
+    BlastRadius,
+    HistoricalContext,
+    RiskAssessment,
+    RiskBreakdown,
+)
+from src.models.test_report import (
+    CoverageReport,
+    FailureClassification,
+    TestAnalysisReport,
+    TestResult,
+)
+
+__all__ = [
+    # build_metadata
+    "BuildInfo",
+    "GitDiff",
+    "PipelineStatus",
+    # test_report
+    "TestResult",
+    "FailureClassification",
+    "CoverageReport",
+    "TestAnalysisReport",
+    # perf_report
+    "BenchmarkResult",
+    "MetricComparison",
+    "SLOCheck",
+    "PerformanceAnalysisReport",
+    # risk_assessment
+    "RiskBreakdown",
+    "BlastRadius",
+    "HistoricalContext",
+    "RiskAssessment",
+    # release_decision
+    "NotificationRecord",
+    "PolicyEvaluation",
+    "ReleaseDecision",
+    "AuditRecord",
+]
